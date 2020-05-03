2e12ff209c912aac2a4346e109a341b1a5f9a10e
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-05-03 18:57:42 +0000

    Updated main reduxstagram.js file

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,38 @@
 import React from 'react';
+import { render } from 'react-dom';
+
+// Styles
+import css from './styles/style.styl';
+
+// Components
+import Main from './components/main';
+import Single from './components/single';
+import PhotoGrid from './components/photo-grid';
+
+// React Router
+import {
+  Router,
+  Route,
+  IndexRoute,
+  browserHistory
+} from 'react-router';
+
+const router = (
+  <Router
+    history={browserHistory}>
+      <Route
+        path="/"
+        component={Main}>
+          <IndexRoute
+            component={PhotoGrid} />
+          <Route
+            path="/view/:postId"
+            component={Single} />
+      </Route>
+  </Router>
+)
+
+render(
+  router,
+  document.getElementById('root')
+);
